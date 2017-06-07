0a6cb27d6a21ce3ab2c1261cb01db98e9003ce06
Owen Pembery <o.r.pembery@bath.ac.uk>
2017-06-07 08:07:28 +0100

    Completed iip test code, although it falls over a lot

--- initial-firedrake-helmholtz-test.py
+++ initial-firedrake-helmholtz-test.py
@@ -5,7 +5,7 @@ d1 = Constant(1)
 d2 = Constant(0)
 
 
-mesh = UnitSquareMesh(64,64)
+mesh = UnitSquareMesh(100,100)
 
 FS1 = FunctionSpace(mesh, "CG", 1)
 FS2 = FunctionSpace(mesh, "CG", 1)
@@ -17,3 +17,15 @@ vR, vI = TestFunctions(V)
 x,y = SpatialCoordinate(mesh)
 fR = Function(FS1).interpolate(cos(omega*(x*d1 + y*d2)))
 fI = Function(FS2).interpolate(sin(omega*(x*d1 + y*d2)))
+
+a = (dot(grad(uR),grad(vR)) - omega*omega * uR * vR + dot(grad(uI),grad(vI)) - omega*omega * uI * vI)*dx# - omega * (uI * vR - uR * vI)*ds
+L = (fR * vR + fI * vI)*dx
+
+w = Function(V)
+
+#solve(a==L, w,solver_parameters={'ksp_type': 'preonly','pc_type': 'lu'})
+#solve(a==L,w)
+solve(a==L, w, solver_parameters={"ksp_type": "gmres"})
+uR, uI = w.split()
+
+File("initial_helmholtz.pvd").write(uR)

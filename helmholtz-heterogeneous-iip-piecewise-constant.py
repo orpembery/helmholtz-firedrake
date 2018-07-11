bbe3523def6a8bed7a01a4c919210d699ef273ab
Owen Pembery <o.r.pembery@bath.ac.uk>
2018-07-11 12:05:26 +0100

    First try and piecewise constant n is working (possibly)

--- helmholtz-heterogeneous-iip-piecewise-constant.py
+++ helmholtz-heterogeneous-iip-piecewise-constant.py
@@ -1,9 +1,14 @@
 # Modified from the firedrake `Simple Helmholtz equation' demo
 from firedrake import *
 from math import ceil # so that we can define the mesh size
+import numpy as np
 
 # Define wavenumber
 k = 10.0
+
+# Define number of `pieces' in piecewise constant coefficients - double
+
+coeff_pieces = 2
 
 # Define mesh size to eliminate pollution effect
 mesh_size = ceil(k**(1.5))
@@ -28,9 +33,34 @@ d = as_vector([1.0/sqrt(2.0),1.0/sqrt(2.0)])
 g=1j*k*exp(1j*k*dot(x,d))*(dot(d,nu)-1)
 
 # Define coefficients
-A = as_matrix([[1.0,0.0],[0.0,1.0]])
 
-n = 1.0
+def heaviside(x): # x here is a single coordinate of a SpatialCoordinate
+  return 0.5 * (sign(real(x)) + 1.0)
+
+def Iab(x,a,b) : # indicator function on [a,b] - x is a single coordinate of a spatial coordinate, 0.0  <= a < b <= 1 are doubles
+  return 0.5 * ( heaviside(x-a) + 1.0 - heaviside(x-b) )
+
+n = 1.0 # background
+
+n_values = 10.0 * np.array([[0.1,-0.05],[-0.03,0.08]])
+
+for xii in range(0,coeff_pieces-1):
+  for yii in range(0,coeff_pieces-1):
+    n += n_values[xii,yii] * Iab(x[0],xii/coeff_pieces,(xii+1)/coeff_pieces) * Iab(x[1],yii/coeff_pieces,(yii+1)/coeff_pieces)
+
+# Plot n
+
+n_func = Function(V)
+
+n_func.interpolate(n)
+
+import matplotlib.pyplot as plt
+
+plot(n_func,num_sample_points=1)
+
+plt.show()
+
+A = as_matrix([[1.0,0.0],[0.0,1.0]])
 
 # Define sesquilinear form and antilinear functional
 a = (inner(A * grad(u), grad(v)) - k**2 * inner(real(n) * u,v)) * dx - (1j* k * inner(u,v)) * ds # real(n) is just a failsafe
@@ -51,6 +81,12 @@ try:
 except:
   warning("Matplotlib not imported")
 
+#try:
+#  plot(mesh)
+#  plt.show()
+#except:
+#  warning("Dunno")
+  
 try:
   plot(u_h,num_sample_points=1)
 except Exception as e:

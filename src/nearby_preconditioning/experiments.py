c5b1fff5c0ddd5ae8c2c5fe7fbb347c437a81ff1
Owen Pembery <o.r.pembery@bath.ac.uk>
2018-09-05 17:00:53 +0100

    Moved code around to make it easier to find (in wrong branch, oops) and started to write test code for nbpc paper.

--- src/nearby_preconditioning/experiments.py
+++ src/nearby_preconditioning/experiments.py
@@ -1,5 +1,6 @@
 import firedrake as fd
 import helmholtz.problems as hh
+import helmholtz.coefficients as coeff
 import numpy as np
 import subprocess
 import datetime
@@ -82,7 +83,8 @@ def nearby_preconditioning_piecewise_test_set(
 
     n_pre_type - string - options are 'constant', giving n_pre = 1.0.
 
-    num_pieces - see PieceWiseConstantCoeffGenerator.
+    num_pieces - see
+        helmholtz.coefficients.PieceWiseConstantCoeffGenerator.
 
     seed - see StochasticHelmholtzProblem.
 
@@ -209,9 +211,9 @@ def nearby_preconditioning_piecewise_test_set(
                     n_modifier = h ** modifier[2] * k**modifier[3]
                     A_noise_level = A_noise_master * A_modifier
                     n_noise_level = n_noise_master * n_modifier
-                    A_stoch = PiecewiseConstantCoeffGenerator(
+                    A_stoch = coeff.PiecewiseConstantCoeffGenerator(
                         mesh,num_pieces,A_noise_level,A_pre,[2,2])
-                    n_stoch = PiecewiseConstantCoeffGenerator(
+                    n_stoch = coeff.PiecewiseConstantCoeffGenerator(
                         mesh,num_pieces,n_noise_level,n_pre,[1])
                     np.random.seed(seed)
                     
@@ -230,307 +232,6 @@ def nearby_preconditioning_piecewise_test_set(
                          'num_repeats' : num_repeats
                          }
                         )
-def h_to_mesh_points(h):
-    """Converts a mesh size to a number of points giving that mesh size.
-
-    Given a mesh size h, computes the arguments to Firedrake's
-    UnitSquareMesh that will give (at most) that mesh size in 2D.
-
-    Parameter:
-
-    h - positive float - the mesh size.
-    """
-    return np.ceil(np.sqrt(2.0)/h)
-
-def write_GMRES_its(GMRES_its,save_location,info):
-    """Writes the number of GMRES iterations, and other information, to
-    a .csv file.
-
-    Parameters:
-
-    save_location - see nearby_preconditioning_test_set.
-
-    GMRES_its - list of positive ints of length num_repeats (output of
-    nearby_preconditioning_test).
-
-    info - a dict containing all of the other information to be written
-    to the file.
-
-    The output csv file will have the filename
-    'nearby-preconditioning-test-output-date_time.csv, where date_time
-    is the date and time. The rows of the file will consist of the hash
-    of the current git commit, then the date and time, then all of the
-    entries of info (where the value first column will be the key, and
-    the value in the second column will be the value in the dict),
-    followed by the GMRES iterations (repeat number in the first column,
-    number of GMRES iterations in the second).
-    """
-
-    # Check save_location is actually a directory path
-    assert save_location[-1] == "/"
-    
-    # Get git hash
-    git_hash = subprocess.run("git rev-parse HEAD", shell=True,
-                              stdout=subprocess.PIPE)
-    # help from https://stackoverflow.com/a/6273618
-    git_hash_string = git_hash.stdout.decode('UTF-8')[:-1]
-
-    # Get current date and time
-    # This initialises the object. I don't understand why this is
-    # necessary.
-    date_time = datetime.datetime(1,1,1)
-    date_time = date_time.utcnow().isoformat()
-  
-    # Write CSV
-    # from https://docs.python.org/3.5/library/csv.html
-    with open(save_location + 'nearby-preconditioning-test-output-'
-              + date_time + '.csv', 'w', newline = '') as csvfile:
-        file_writer = csv.writer(csvfile, delimiter = ',',
-                                 quoting = csv.QUOTE_MINIMAL)
-        file_writer.writerow(['Git hash', git_hash_string])
-
-        # Current time in UTC as an ISO string
-        file_writer.writerow(['Date/Time', date_time])
-
-        # All other properties
-        for ii in info.keys():
-            file_writer.writerow([ii,info[ii]])
-
-        for ii in range(len(GMRES_its)):
-            file_writer.writerow([ii,GMRES_its[ii]])
-        print(save_location)
-class PiecewiseConstantCoeffGenerator(object):
-    """Does the work of A_stoch and n_stoch in
-    StochasticHelmholtzProblem for the case of piecewise continuous on
-    some grid.
-
-    Floats are Unif(noise_level,noise_level) perturbations
-    of coeff_pre (see init).
-
-    Matrices (call them A) are generated so that if coeff_pre is the 2x2
-    identity, then the entrywise L^\infty norm of A-I is <= noise_level
-    almost surely, and the matrices are s.p.d..
-
-    The method for construction can almost certainly be made better, but
-    is a bit of a hack until they've sorted complex so that I can do
-    things the way Julian does.
-
-    Attribute:
-
-    coeff - a UFL expression for a piecewise constant (scalar- or
-    matrix-valued) coefficient, implemented using Firedrake Constants.
-
-    Method:
-
-    sample - randomly updates coeff by randomly sampling around the
-    known background given by the input argument coeff_pre. Samples have
-    (entrywise) L^\infty norm <= noise_level almost surely.
-    """
-
-    def __init__(self,mesh,num_pieces,noise_level,coeff_pre,coeff_dims):
-        """Initialises a piecewise-constant random coefficient, where
-        the coefficient is peicewise-constant on a num_pieces x
-        num_pieces grid.
-
-        Parameters:
-
-        mesh - a Firedrake mesh object.
-
-        num_pieces - int - the number of `pieces' in each direction for
-        the piecewise-constant coefficients. Empirically must be <= 13,
-        or get a recursion error.
-
-        noise_level - positive float - the level of the random noise.
-
-        coeff_pre - a UFL expression with dimension coeff_dims - the
-        background around which we take piecewise-constant random
-        perturbations.
-
-        coeff_dims - a list, either [1] or [2,2] - the `dimension' of
-        the coefficient (i.e., either scalar-valued or 2x2 matrix
-        valued).
-        """
-
-        self._coeff_dims = coeff_dims
-        """Dimension of the coefficient - [1] or [2,2]."""
-
-        self._num_pieces = num_pieces
-        """Number of pieces in each direction\
-        for random coefficients."""
-
-        self._noise_level = noise_level
-        """Magnitude of random noise."""
-                                                  
-        self._coeff_initialise(mesh,coeff_pre)
-        """Create realisation of random coefficient."""
-
-        self.sample()
-        
-
-    def _list_extract(self,values_list,x_coord,y_coord,coord_length):
-        """If values_list were put into a coord_length x
-        coord_length array, extracts the item at position
-        (x_coord,y_coord).
-        
-        Parameters:
-        
-        values_list - list of length coord_length**2.
-
-        x_coord, y_coord - int in range(coord_length).
-
-        coord_length - int.
-        """
-        return values_list[x_coord + y_coord * coord_length]
-
-    def _heaviside(self,x):
-        """Defines the heaviside step function in UFL.
-
-        Parameters:
-
-        x - single coordinate of a UFL SpatialCoordinate.
-        """
-        return 0.5 * (fd.sign(fd.real(x)) + 1.0)
-
-    def _Iab(self,x,a,b) :
-        """Indicator function on [a,b] in UFL.
-
-        Parameters:
-
-        x - single coordinate of a UFL SpatialCoordinate.
-        
-        a, b - floats in [0,1].
-        """
-        return self._heaviside(x-a) - self._heaviside(x-b)
-
-    def _coeff_initialise(self,mesh,coeff_pre):
-        """Initialises self.coeff equal to coeff_pre, but sets up
-        Firedrake Constant structure to allow for sampling.
-
-        Parameters:
-
-        mesh - a Firedrake mesh.
-
-        coeff_pre - see init.
-        """
-
-        if self._coeff_dims == [2,2]\
-                and coeff_pre != fd.as_matrix([[1.0,0.0],[0.0,1.0]]):
-
-            warnings.warn("coeff_pre is not the identity. There is not\
-            guarantee that the randomly-generated matrices are\
-            positive-definite, or have the correct amount of noise.")
-        
-        self._coeff_values = []
-        
-        for ii in range(self._num_pieces**2):
-            if self._coeff_dims == [2,2]:
-                self._coeff_values.append(np.array([[0.0,0.0],[0.0,0.0]]))
-            elif self._coeff_dims == [1]:
-                self._coeff_values.append(np.array(0.0))
-            else:
-                raise NotImplementedError(
-                          "Have only implemented real- and\
-                          matrix-valued coefficients")
-                
-        self._coeff_values = [fd.Constant(coeff_dummy,domain=mesh)
-                              for coeff_dummy in self._coeff_values]
-        
-        # Form coeff by looping over all the subdomains
-        x = fd.SpatialCoordinate(mesh)
-
-        self.coeff = coeff_pre
-        
-        for xii in range(self._num_pieces):
-            for yii in range(self._num_pieces):
-                self.coeff +=\
-                self._list_extract(self._coeff_values,xii,yii,
-                                   self._num_pieces)\
-                * self._Iab(x[0],xii/self._num_pieces,(xii+1)/
-                            self._num_pieces)\
-                * self._Iab(x[1],yii/self._num_pieces,(yii+1)/self._num_pieces)
-
-    def sample(self):
-        """Samples the coefficient coeff."""
-        [coeff_dummy.assign(self._generate_matrix_coeff())
-         for coeff_dummy in self._coeff_values]
-
-    def _generate_matrix_coeff(self):
-        """Generates a realisation of the random coefficient.
-
-        For matrices, uses the fact that for real matrices,
-        [[1+a,b],[b,1+c]] is positive-definite iff 1+a>0 and b**2 <
-        (1+a)*(1+c).
-
-        Let L denote self._noise_level. Matrices are of the form A =
-        coeff_pre + [[a,b],[b,c]], where a,c ~ Unif(-L,0) and, letting
-        b_bound = min{L,sqrt((1+a)*(1+c))}, b ~ Unif(-b_bound,b_bound)
-        with a,b,c independent of each other. This construction
-        guarantees that if coeff_pre = I, then A is s.p.d. and the
-        entrywise L^\infty norm of A-I is bounded by self._noise_level
-        almost surely.
-        """
-
-        if self._coeff_dims == [1]:
-            coeff = self._noise_level*(
-                2.0 * np.random.random_sample(self._coeff_dims) - 1.0)
-        elif self._coeff_dims == [2,2]:
-            a = -self._noise_level * np.random.random_sample(1)
-            c = -self._noise_level * np.random.random_sample(1)
-            b_bound = min(self._noise_level,np.sqrt((1.0+a)*(1.0+c)))
-            b = b_bound * (2.0 * np.random.random_sample(1) - 1.0)
-
-            coeff = np.array([[a,b],[b,c]])
-        return coeff
-
-
-class GammaConstantCoeffGenerator(object):
-    """Does the work of n_stoch in StochasticHelmholtzProblem for the
-    case of a constant but gamma-distributed refractive index.
-
-    Attribute:
-
-    coeff - a Firedrake Constant containing the value of the
-    scalar-valued coefficients.
-
-    Method:
-
-    sample - randomly updates coeff by randomly sampling from coeff_base
-    + gamma(rate).
-    """
-
-    def __init__(self,shape,scale,coeff_lower_bound):
-        """Initialises a constant, gamma-distributed constant.
-
-        Parameters:
-
-        shape - the shape of the gamma distribution (commonly called k).
-
-        scale - the scale of the gamma distribution (commonly
-        called theta).
-
-        coeff_lower_bound - the lower bound for the coefficient.
-
-        The mean of the gamma distribution is shape * scale and the
-        variance is shape * scale**2.
-        """
-
-        self._coeff_rand = fd.Constant(0.0)
-
-        self._coeff_lower_bound = coeff_lower_bound
-
-        self.coeff = self._coeff_lower_bound + self._coeff_rand
-        """Spatially homogeneous, but random coefficient."""
-
-        self._shape = shape
-        
-        self._scale = scale
-        
-        self.sample()
-
-    def sample(self):
-        """Samples the exponentially-distributed coefficient coeff."""
-
-        self._coeff_rand.assign(np.random.gamma(self._shape,self._scale))
 
 def nearby_preconditioning_test_gamma(k_range,n_lower_bound,n_var_base,
                                       n_var_k_power_range,num_repeats):
@@ -560,7 +261,8 @@ def nearby_preconditioning_test_gamma(k_range,n_lower_bound,n_var_base,
             scale = n_var / (1.0 - n_lower_bound)
             shape = (1.0 - n_lower_bound)**2 / n_var
             
-            n_stoch = GammaConstantCoeffGenerator(shape,scale,n_lower_bound)
+            n_stoch = coeff.GammaConstantCoeffGenerator(
+                shape,scale,n_lower_bound)
 
             n_pre = 1.0
             f = 0.0
@@ -589,3 +291,79 @@ def nearby_preconditioning_test_gamma(k_range,n_lower_bound,n_var_base,
                     
             
             write_GMRES_its(GMRES_its,save_location,info)
+
+def special_rhs_for_paper_test(k_list,num_system,num_rhs,fine_grid_refinement):
+    """Tests to see if the required condition in the paper holds.
+
+    For a variety of different Helmholtz systems, and a variety of
+    different special right-hand sides (given by F(v) = (A_rhs
+    \grad(\sum_i \alpha_i \phi_i),\grad v)_{L^2}, where the phi_i are
+    the basis functions for piecewise-affine finite-elements, records
+    the weighted H^1 norm of the finite-element error (approximated by
+    taking the solution on a much finer grid) and the norm of the
+    right-hand side in (H^1_k)'.
+
+    Parameters:
+
+    k_list - list of positive integers - the values of k for which
+    experiments will be done.
+
+    num_system - positive integer - the number of different system
+    matrices (i.e. A,n) for which to perform experiments.
+
+    num_rhs - positive integer - the number of different right-hand
+    sides (for each different system) for which to perform experiments.
+
+    fine_grid_refinement - positive integer - the `true' solution is
+    computed on a very fine grid - this will be a
+    fine_grid_refinement-fold uniform refinement of the finest grid used
+    in any of the computations. (E.g., if fine_grid_refinement = 3, the
+    the grid for computing the `true' solution will have a mesh size
+    2^{-3} = 3-times smaller than the finest grid used in any of the
+    computations.)
+    """
+
+    h_power_list = [-1.5,-2.0]
+
+    seed = 1.0
+    
+    smallest_mesh_size_num_points =
+        h_to_mesh_points(max(k_list)**min(h_power_list))
+
+    np.random.seed(seed)
+        
+    for k in k_list:
+
+        for h_power in h_power_list:
+
+            storage = np.full((num_system*num_rhs,2),-1.0)
+            
+            # Set up problem so that it can be easily altered
+
+            # For now, create a separate class that implements a very simple A and n, so that a more complicated one can always be substituted in later.
+            
+            for ii_system in range(num_system):
+
+                # Set up `fine' problem
+                
+                # Sample system and update problem
+                
+                for ii_rhs in range(num_rhs):
+
+                    # Sample rhs
+
+                    # Compute fine problem
+
+                    # Compute coarse problem
+
+                    # transfer coarse solution to fine mesh
+
+                    # measure weighted H^1 norm of error
+
+                    # measure L^2 norm of rhs as proxy
+
+                    # store error and rhs
+
+                    
+
+            

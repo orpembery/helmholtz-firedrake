12dfd0fd76b03a731c471d2fc965c518284a2a9d
Owen Pembery <o.r.pembery@bath.ac.uk>
2018-09-25 15:32:30 +0100

    Updated nbpc run script.

--- scripts/nbpc-experiments/run_generic_nbpc_experiment.py
+++ scripts/nbpc-experiments/run_generic_nbpc_experiment.py
@@ -4,7 +4,7 @@ A_pre_type = 'constant'
 
 n_pre_type = 'constant'
 
-num_pieces = 2
+num_pieces = 10
 
 seed = 1
 
@@ -16,8 +16,12 @@ h_list = [(1.0,-1.0),(1.0,-1.5),(1.0,-2.0)]
 
 noise_master_level_list = [(0.1,0.0)]
 
-noise_modifier_list = [(0.0,-1.0,0.0,0.0),(1.0,0.0,0.0,0.0)]
+#noise_modifier_list = [(0.0,-1.0,0.0,0.0),(1.0,0.0,0.0,0.0)]
 
-save_location = '/home/owen/Documents/code/helmholtz-firedrake/output-to-copy-to-x/nbpc-paper/initial-test-of-A-condition/'
+noise_modifier_list = [(0.0,0.0,0.0,0.0)]
+
+#save_location = '/home/owen/code/helmholtz-firedrake/output-to-copy-to-x/nbpc-paper/initial-test-of-A-condition/' # laptop
+
+save_location = '/home/orp20/code/helmholtz-firedrake/output-to-copy-to-x/nbpc-paper/initial-test-of-A-condition/' # uni desktop
 
 nbpc.nearby_preconditioning_piecewise_experiment_set(A_pre_type,n_pre_type,num_pieces,seed,num_repeats,k_list,h_list,noise_master_level_list,noise_modifier_list,save_location)

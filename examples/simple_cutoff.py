805e5bdf603530f207ed901014e5dafacd3120a6
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-04-26 14:52:56 +0100

    Sharp cutoff appears (by eye) to be working, as does the boundary condition for scattering by compactly supported hetergeneities.

--- examples/simple_cutoff.py
+++ examples/simple_cutoff.py
@@ -22,7 +22,8 @@ V = FunctionSpace(mesh,"CG",1)
 # n = 1.5 + sin(x*y)
 x = SpatialCoordinate(mesh)
 square_limits = np.array([[1.0/3.0,2.0/3.0],[1.0/3.0,2.0/3.0]])
-n = 1.5 + sin(x[0]*x[1])
+n_osc = 30.0
+n = 1.5 + sin(n_osc*x[0])*sin(n_osc*x[1])
 
 # Define the problem
 prob = hh.HelmholtzProblem(k,V,n=n)
@@ -33,12 +34,14 @@ centre = np.array((0.5,0.5))
 width = 0.5
 transition_zone_width = 0.2
 
-prob.n_cutoff(centre,width,transition_zone_width)
-
-
 # Use f and g corresponding to a plane wave (in homogeneous media)
-angle = np.pi/3.0
-prob.f_g_plane_wave([np.cos(angle),np.sin(angle)])
+angle = 2.0*np.pi/3.0
+
+prob.sharp_cutoff(np.array([0.5,0.5]),0.6)
+
+prob.plot_n()
+
+prob.f_g_scattered_plane_wave([np.cos(angle),np.sin(angle)])
 
 # Use MUMPS as a direct solver
 prob.use_mumps()

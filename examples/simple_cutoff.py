ddb934bfb9e48084de84bd46df2c862b3bcafd9c
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-04-26 11:45:35 +0100

    Example with cutoff function added. Possible issues with tsfc

---
+++ examples/simple_cutoff.py
@@ -0,0 +1,50 @@
+# Assumes this code is being run from the 'examples' folder. Otherwise,
+# add the helmholtz_firedrake folder to your PYTHONPATH
+import sys
+sys.path.append('../')
+from firedrake import *
+import numpy as np
+from helmholtz_firedrake import problems as hh
+from helmholtz_firedrake.utils import h_to_num_cells, nd_indicator
+
+k = 30.0
+
+# Define a mesh that keeps pollution error bounded
+num_cells = h_to_num_cells(k**-1.5,2)
+
+L = 1.0
+
+mesh = SquareMesh(num_cells,num_cells,L)
+
+# Use piecewise-linear finite elements
+V = FunctionSpace(mesh,"CG",1)
+
+# n = 1.5 + sin(x*y)
+x = SpatialCoordinate(mesh)
+square_limits = np.array([[1.0/3.0,2.0/3.0],[1.0/3.0,2.0/3.0]])
+n = 1.5 + sin(x[0]*x[1])
+
+# Define the problem
+prob = hh.HelmholtzProblem(k,V,n=n)
+
+
+# n is modified so that it decays to 1 outside a square
+centre = np.array((0.5,0.5))
+width = 0.5
+transition_zone_width = 0.2
+
+prob.n_cutoff(centre,width,transition_zone_width)
+
+
+# Use f and g corresponding to a plane wave (in homogeneous media)
+angle = np.pi/3.0
+prob.f_g_plane_wave([np.cos(angle),np.sin(angle)])
+
+# Use MUMPS as a direct solver
+prob.use_mumps()
+
+# Solve the problem
+prob.solve()
+
+# Plot the solution
+prob.plot()
